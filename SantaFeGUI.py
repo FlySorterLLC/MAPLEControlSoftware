79818e0de98365dd961a0199147e80a0bf49e327
flysorter <dave@flysorter.com>
2015-06-05 16:44:04 -0400

    More steps along the way
    Getting things up and running

--- SantaFeGUI.py
+++ SantaFeGUI.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
 
-## Copyright (c) 2014, FlySorter, LLC
+## Copyright (c) 2015, FlySorter, LLC
 
 import cv2
 import numpy as np
+import time
 import ConfigParser
 
 import robotutil
@@ -11,8 +12,7 @@ import robotutil
 ## Some defaults & settings
 
 # Configuration defaults
-configDefaults = {'COM': '2',
-                  'CamIndex': '2',
+configDefaults = {'CamIndex': '0',
                   'OutputDir': 'C:/Users/DaveZ/Documents/Training Data',
                   }
 
@@ -42,8 +42,47 @@ robot = robotutil.santaFe(CamIndex)
 if robot.isInitialized == False:
     print "Initialization error."
     exit()
+else:
+    print "Robot initialized."
 
-# Close up shop, write config
+
+key = -1
+
+# Move to lid
+robot.moveTo( ( 425., 125., 0, 0, 0 ) )
+robot.moveTo( ( 425., 125., 69.5, 0, 0) )
+# Pick lid
+robot.vacuum(True)
+time.sleep(1)
+
+# Move lid up
+robot.moveTo( ( 425., 125., 40.0, 0, 0) )
+# Move to fly
+robot.moveTo( ( 525., 50., 40.0, 0, 0 ) )
+
+robot.moveTo( ( 525., 50., 40.0, 0, 29.75 ) )
+robot.moveTo( ( 525., 50., 40.0, 0, 29.75 ) )
+
+# Pick fly
+# (Vac is already on)
+time.sleep(1)
+
+# Move fly up
+robot.moveTo( ( 525.0, 50.0, 40.0, 0, 0 ) )
+
+# Move to chamber
+
+# Deposit fly
+
+# Deposit lid
+
+# Move out of the way
+#robot.vacuum(False)
+print "Done, waiting"
+
+while True:
+    time.sleep(1)
+
 cv2.destroyAllWindows()
 robot.release()
-writeConfig(config)
+# writeConfig(config)
--- robotutil.py
+++ robotutil.py
@@ -1,13 +1,19 @@
+## Copyright (c) 2015, FlySorter LLC
+##
+## This software is licensed under the GPL v2.0
+
 import cv2
 import numpy as np
 import sys
 import glob
 import serial
+import time
 
 class santaFeSerial:
     """Serial class for robot device."""
     
-    WaitTimeout = 10
+    WaitTimeout = 1
+    portName = ""
 
     def __init__(self, port, baud = 9600, timeout = 0.25):
         self.isOpened = False
@@ -22,6 +28,7 @@ class santaFeSerial:
         self.ser.close()
         
     def getSerOutput(self):
+        #print "GSO:"
         output = ''
         while True:
             # read() blocks for the timeout set below *if* there is nothing to read
@@ -32,9 +39,11 @@ class santaFeSerial:
             output += byte
             if byte == '\n':
                 break
+        #print "GSO Output:", output
         return output
 
     def waitForOK(self):
+        #print "WFO:"
         output = ''
         timeoutMax = self.WaitTimeout / self.ser.timeout
         timeoutCount = 0
@@ -49,30 +58,28 @@ class santaFeSerial:
                 break
             if byte == '\n':
                 break
-        if output != 'ok\r\n':
-            print "Got something other than 'ok' from serial: ", ':'.join(x.encode('hex') for x in output)
+        #print "WFO Output:", output
+        if ( not output.startswith("ok") ) and ( not output.startswith("OK") ):
+            print "Unexpected serial output:", output.rstrip('\r\n'), "(", ':'.join(x.encode('hex') for x in output), ")"
 
     # Send a command to the device via serial port
     # Asynchronous by default - doesn't wait for reply
     def sendCmd(self, cmd):
+        #print "SC:", cmd
         self.ser.write(cmd)
         self.ser.flush()
-        self.getSerOutput()
 
     # Send a command to the device via serial port
     def sendSyncCmd(self, cmd):
-        serOutput = self.getSerOutput()
-        while serOutput != '':
-            serOutput = self.getSerOutput()
+        #print "SSC:", cmd
+        self.ser.flushInput()
         self.ser.write(cmd)
         self.ser.flush()
         self.waitForOK()
 
     # Send a command and return the reply
     def sendCmdGetReply(self, cmd):
-        serOutput = self.getSerOutput()
-        while serOutput != '':
-            serOutput = self.getSerOutput()
+        self.ser.flushInput()
         self.ser.write(cmd)
         self.ser.flush()
         return self.getSerOutput()
@@ -85,100 +92,208 @@ class santaFe:
     """Class for fly manipulation robot."""
 
     # Constants
-    safeZHeight = 40
 
-    pxPerMM = np.float32([ 2592.0/7.0, 1944.0/5.4 ])
+    
+    # There are 6 encoder counts per revolution, gear ratio of motor is 29.86:1,
+    # leadscrew pitch is 1/10.4" and there are 25.4 mm/in
+    ZCountsPerMM = 6.0*29.86*10.4/25.4 # = 79.36
+    ZAddressBase = 54
+
+    printrboardPort = ""
+    synaptronsPort = ""
+
     imageCenter = np.float32( [2592.0/2.0, 1944.0/2.0 ])
+
+    currentPosition = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+    currentRotation = np.array([0.0, 0.0])
 
     isInitialized = False
     
     def __init__(self, camIndex):
+        print "Initializing serial connections..."
         self.printrboard = None
         self.synaptrons = None
         # Search serial ports, look for motor control board and printrboard
         portList = availablePorts()
+        print "Port list:", portList
         for portDesc in portList:
-            print "Trying port", portDesc
+            # print "Trying port", portDesc
             tempPort = santaFeSerial(portDesc)
             if tempPort.sendCmdGetReply("M115\r\n").startswith("FIRMWARE_NAME"):
-                print "Port", portDesc, "is printrboard."
+                print "Port:", portDesc, "is printrboard."
+                self.printrboardPort = portDesc
                 self.printrboard = tempPort
-                break
+                continue
             if tempPort.sendCmdGetReply("54,00,\r\n").startswith("54"):
-                print "Port", portDesc, "is motor controller."
+                print "Port:", portDesc, "is motor controller."
+                self.synaptronsPort = portDesc
                 self.synaptrons = tempPort
-                break
+                continue
             tempPort.close()
 
-        if self.printrboard is None:
-            print "Printrboard not found"
-        if self.synaptrons is None:
-            print "Synaptrons not found"
-        print self.synaptrons.sendCmdGetReply("54,00,\r\n")
-        #self.cam = cameraInit(camIndex)
-        #if self.cam == None:
-        #    print "Camera init fail."
-        #    self.serialPort.close()
-        #    return
+        if self.printrboard is None or self.synaptrons is None:
+            print "Serial initialization failed."
+            if self.printrboard is None:
+                print "Printrboard not found."
+            else:
+                self.printrboard.close()
+            if self.synaptrons is None:
+                print "Synaptrons not found."
+            else:
+                self.synaptrons.close()
+            return
 
+        # Temporary hack! Synaptrons don't like when you switch
+        # between the different addresses, so we open & close the port
+        # for each operation.
+        self.synaptrons.close()
+
+        self.cam = cameraInit(camIndex)
+        if self.cam == None:
+            print "Camera init fail."
+            self.synaptrons.close()
+            self.printrboard.close()
+            return
+
+        print "Homing..."
+        self.home()
+        self.dwell(1)
+        self.printrboard.sendSyncCmd("G01 F5000\n")
+        
         self.isInitialized = True
+        return
 
     def release(self):
-        #self.cam.release()
+        self.cam.release()
         self.printrboard.close()
         self.synaptrons.close()
 
     def home(self):
-        cmd = "G28 Y\n"
-        self.printrboard.sendSyncCmd(cmd)
-        cmd = "G28 X\n"
-        self.printrboard.sendSyncCmd(cmd)
-        # Now home 
-    
-    def moveTo(self, pt):
-        cmd = "G01 Z{0}\n".format(self.safeZHeight)
-        self.printrboard.sendCmd(cmd)
-        cmd = "G01 X{0[0]:.4} Y{0[1]:.4} Z{0[2]:.4}\n".format(pt)
-        self.printrboard.sendCmd(cmd)
+        # First home 3 small motors for z axes
+        self.homeZAxis(0)
+        self.homeZAxis(1)
+        self.homeZAxis(2)
+        self.printrboard.sendSyncCmd("G28 Y\n")
+        self.printrboard.sendSyncCmd("G28 X\n")
+        return
+        
+    def homeZAxis(self, zNum):
+        # To home the Z axes, move it all the way up,
+        # wait for the limit switch to be engaged,
+        # disable motor, reset position and setpoint,
+        # re-enable motor
+        print "Homing Z axis", zNum
+        # TEMPORARY HACK
+        self.synaptrons = santaFeSerial(self.synaptronsPort)
+        address = self.ZAddressBase+zNum
+        cmd = "{0},39,-7000\r\n".format(address)
+        self.synaptrons.sendSyncCmd(cmd)
+        # Better than sleeping, we should poll the controller
+        # until the switch is engaged, with a timeout.
+        time.sleep(5)
+        self.synaptrons.sendSyncCmd("{0},03,22\r\n".format(address))
+        self.synaptrons.sendSyncCmd("{0},05,0\r\n".format(address))
+        self.synaptrons.sendSyncCmd("{0},39,0\r\n".format(address))
+        self.synaptrons.sendSyncCmd("{0},03,6\r\n".format(address))
+        # TEMPORARY HACK
+        self.synaptrons.close()
+        return
 
-    def unsafeMove(self, pt):
-        cmd = "G01 X{0[0]:.4} Y{0[1]:.4} Z{0[2]:.4}\n".format(pt)
-        self.printrboard.sendCmd(cmd)
-    
+    def moveTo(self, pt):
+        self.currentPosition = pt
+        cmd = "G01 X{0[0]:.4} Y{0[1]:.4}\n".format(pt)
+        self.printrboard.sendSyncCmd(cmd)
+        self.dwell(1)
+
+        time.sleep(0.5)
+
+        # TEMPORARY HACK
+        self.synaptrons = santaFeSerial(self.synaptronsPort)
+        self.synaptrons.sendSyncCmd("54,39,{0}\r\n".format(int(pt[2]*self.ZCountsPerMM)))
+        # TEMPORARY HACK
+        self.synaptrons.close()
+
+        time.sleep(0.5)
+        
+        # TEMPORARY HACK
+        self.synaptrons = santaFeSerial(self.synaptronsPort)
+        self.synaptrons.sendSyncCmd("55,39,{0}\r\n".format(int(pt[3]*self.ZCountsPerMM)))
+        # TEMPORARY HACK
+        self.synaptrons.close()
+
+        time.sleep(0.5)
+
+        # TEMPORARY HACK
+        self.synaptrons = santaFeSerial(self.synaptronsPort)
+        self.synaptrons.sendSyncCmd("56,39,{0}\r\n".format(int(pt[4]*self.ZCountsPerMM)))
+        # TEMPORARY HACK
+        self.synaptrons.close()
+
+        time.sleep(0.5)
+
+        return
+
     def dwell(self, t):
         cmd = "G04 P{0}\n".format(t)
         self.printrboard.sendSyncCmd(cmd)
+        return
 
-    def light(self, LEDs = 'vis', onOff = False):
-        if (LEDs == 'vis'):
-            if ( onOff == True ):
-                cmd = "M106\n"
-            else:
-                cmd = "M107\n"
-            self.printrboard.sendCmd(cmd)
+    def light(self, onOff = False):
+        if ( onOff == True ):
+            cmd = "M106\n"
         else:
-            print "Don't understand how to turn on LED:", LEDs
-            return
-
-    def vacuum(self, onOff = False, port = 0):
-        if (port == 0):
-            if (onOff == True):
-                cmd = "M42 P14 S255\n"
-            else:
-                cmd = "M42 P14 S0\n"
-        elif (port == 1):
-            if (onOff == True):
-                cmd = "M42 P15 S255\n"
-            else:
-                cmd = "M42 P15 S0\n"
+            cmd = "M107\n"
         self.printrboard.sendCmd(cmd)
+        return
 
+    def vacuum(self, onOff = False):
+        if (onOff == True):
+            cmd = "M42 P12 S255\n"
+        else:
+            cmd = "M42 P12 S0\n"
+        self.printrboard.sendCmd(cmd)
+        return
+
+    def air(self, onOff = False):
+        if (onOff == True):
+            cmd = "M42 P11 S255\n"
+        else:
+            cmd = "M42 P11 S0\n"
+        self.printrboard.sendCmd(cmd)
+        return
+
+    def smallPartManip(self, onOff = False):
+        if (onOff == True):
+            cmd = "M42 P9 S255\n"
+        else:
+            cmd = "M42 P9 S0\n"
+        self.printrboard.sendCmd(cmd)
+        return
+
+    def flyManip(self, onOff = False):
+        if (onOff == True):
+            cmd = "M42 P10 S255\n"
+        else:
+            cmd = "M42 P10 S0\n"
+        self.printrboard.sendCmd(cmd)
+        return
+
+    def spinSmallPartManip(self, angle):
+        cmd = "G01 E{0:.4}\n".format(angle)
+        self.printrboard.sendCmd(cmd)
+        return
+
+    def spinFlyManip(self, angle):
+        cmd = "G01 Z{0:.4}\n".format(angle)
+        self.printrboard.sendCmd(cmd)
+        return
 
 
 # Set up close-up camera
 def cameraInit(cam=1):
-    print "Opening close-up cam", cam
+    print "Opening camera interface (", cam, ")"
     close = cv2.VideoCapture(cam)
+    print "Setting properties"
     close.set(cv2.cv.CV_CAP_PROP_EXPOSURE, -4)
     close.set(cv2.cv.CV_CAP_PROP_BRIGHTNESS, 10)
     close.set(cv2.cv.CV_CAP_PROP_GAIN, 5)
